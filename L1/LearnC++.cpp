9b64a3dc6e1757ba30a304b715839272fe8a5ea5
Rishav Raj <79185086+mr-raj2001@users.noreply.github.com>
2024-10-16 17:11:18 +0000

    data

---
+++ L1/LearnC++.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+//     int x;
+//     cin>>x;
+
+//    std::cout<< "Hey Striver!" << "\n";
+
+    //int
+    //long
+
+    //long long
+
+     //float,double
+
+     //string and getline
+     //string str
+     //getline(cin,str);
+     //cout<<str;
+   //int 10^9
+   //long 10^12
+
+   //long long 10^18
+
+   
+     //char 
+     0;
+}
